a2e31bafb6120391962ede6e08e240b3986b8cfb
kuna.yaswanth.kumar <ramya.nanjudappa.gmail.com>
2018-12-06 15:09:41 +0530

    edited test file

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,24 +19,24 @@ public class HelloTest {
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
-		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();
+		/*System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
+		driver.get("http://localhost:9999/Helloworld/");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
 	@Test
 	public void f() {
 		/*
-		text = driver.findElement(By.xpath("html/body/h2"));
+		text = driver.findElement(By.xpath("html/body/h2"));*/
 		Assert.assertEquals(driver.getTitle(), "");
-		System.out.println("Test Passed");*/
-
+		System.out.println("Test Passed");
 	}
 
 	@AfterTest
+	
 	public void afterTest() {
 		//driver.close();
 	}
